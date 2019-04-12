747ba7822ca31d750edc23aca006614b3d170574
tbmsilva <tb.silva@campus.fct.unl.pt>
2019-04-12 23:46:48 +0100

    comments done

--- orders/OrderDelivered.java
+++ orders/OrderDelivered.java
@@ -4,12 +4,32 @@ import bases.*;
 
 public interface OrderDelivered extends Order {
 
+	/**
+	 * Returns tick of when the order was delivered
+	 * 
+	 * @return tick of when the order was delivered
+	 */
 	int timeStamp();
-	
+
+	/**
+	 * Sets the tick of delivery point
+	 * 
+	 * @param tick - tick of delivery
+	 */
 	void setTimeStamp(int tick);
-	
+
+	/**
+	 * Returns the base of departure
+	 * 
+	 * @return the base of departure
+	 */
 	Base origin();
-	
+
+	/**
+	 * Sets the base of departure
+	 * 
+	 * @param origin - base of departure
+	 */
 	void setOrigin(Base origin);
 
 }
